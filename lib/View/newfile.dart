8b422e8f190ad8fb648123114bb8affa67f855a6
Adeel Javed <39371745+adeelghumman@users.noreply.github.com>
2021-09-09 12:32:01 +0500

    2 commit

--- lib/View/newfile.dart
+++ lib/View/newfile.dart
@@ -1,1 +1,12 @@
-first file 
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Newpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('MyPage')),
+      body: Container(child: Text("sas")),
+    );
+  }
+}
